02d782390968c9566bac18d251e77a1780d005f6
Shiyu Wang <shiyu3169@gmail.com>
2019-01-03 11:54:09 -0500

    connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
