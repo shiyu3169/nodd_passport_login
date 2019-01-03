5df9ce40f681ecfdaea3cb23a3522f0344f35556
Shiyu Wang <shiyu3169@gmail.com>
2019-01-03 14:14:08 -0500

    register

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -19,6 +21,28 @@ mongoose
 // EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 
 // Routes
 app.use("/", require("./routes"));
--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: True },
-  email: { type: String, required: True },
-  password: { type: String, required: True },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
