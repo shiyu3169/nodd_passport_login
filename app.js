32a63d25a7344bbb2596bdd5a4a8d1f711617841
Shiyu Wang <shiyu3169@gmail.com>
2019-01-03 11:46:35 -0500

    views

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", require("./routes"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
